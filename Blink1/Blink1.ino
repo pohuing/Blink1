d5e9e94783a62f0424cf6dc8a59f0ce730f19c26
pohuing <24683548+pohuing@users.noreply.github.com>
2024-03-21 23:49:54 +0100

    krams

--- Blink1/Blink1.ino
+++ Blink1/Blink1.ino
@@ -54,22 +54,75 @@ struct Datagramm
 	byte __[4];
 };
 
+
 struct DetectionDatagramm {
 	byte frameHeader[4]{ 0xF4, 0xF3, 0xF2, 0xF1 };
 
-	short frameDataLength;
+	 short frameDataLength;
 	DataType type;
 	byte head{ 0xAA };
 	TargetState targetState;
-	short distance;
+	 short distance;
 	byte exerciseTargetEnergyValue;
-	short stationaryTargetDistance1;
+	 short stationaryTargetDistance1;
 	byte stationaryTargetEnergyValue;
-	short detectionDistanceCm;
+	 short detectionDistanceCm;
 	byte tail{ 0x55 };
 	byte check{ 0x00 };
 
 	byte MFR[4]{0xF8,0xF7, 0xF6, 0xF5 };
+
+	static bool tryRead(const byte* buffer, size_t bufferSize, DetectionDatagramm* out) {
+		size_t window = 0;
+		while (window + sizeof(DetectionDatagramm) < bufferSize) {
+			if (buffer[window] == 0xF4 && buffer[window +1 ] == 0xF3 && buffer[window +2] == 0xF2 && buffer[window+3] == 0xF1)
+			{
+
+				//Serial.println("Found frameHeader");
+				//unsigned short length = buffer[window + offsetof(DetectionDatagramm, frameDataLength) +1 ] << 8 | buffer[window + offsetof(DetectionDatagramm, frameDataLength)];
+				//Serial.print("Got Length: ");
+				//Serial.println(length);
+
+				if (buffer[window + offsetof(DetectionDatagramm, head)] != 0xAA)
+				{
+					//Serial.println("Missing head at: ");
+					//printBytes(buffer, bufferSize, window + offsetof(DetectionDatagramm, head));
+					return false;
+				}
+
+				if (buffer[window + offsetof(DetectionDatagramm, tail)] != 0x55) {
+					//Serial.print("Missing Tail at: ");
+					//Serial.println(window + offsetof(DetectionDatagramm, tail));
+					//printBytes(buffer, bufferSize, window + offsetof(DetectionDatagramm, tail));
+					return false;
+				}		
+
+				if (buffer[window + offsetof(DetectionDatagramm, check)] != 0x00) {
+					//Serial.print("Missing Check at: ");
+					//Serial.println(window + offsetof(DetectionDatagramm, check));
+					//printBytes(buffer, bufferSize, window + offsetof(DetectionDatagramm, check));
+					return false;
+				}
+
+				if (buffer[window + offsetof(DetectionDatagramm, MFR)] != 0xF8 || 
+					buffer[window + offsetof(DetectionDatagramm, MFR) + 1] != 0xF7 || 
+					buffer[window + offsetof(DetectionDatagramm, MFR) + 2] != 0xF6 || 
+					buffer[window + offsetof(DetectionDatagramm, MFR) + 3] != 0xF5)
+				{
+					//Serial.print("Missing MFR at:");
+					//printBytes(buffer, bufferSize, window + offsetof(DetectionDatagramm, MFR));
+					return false;
+				}
+
+				memcpy(out, buffer + window, sizeof(DetectionDatagramm));
+				return true;
+			}
+			else {
+				window++;
+			}
+		}
+		return false;
+	}
 
 	void prettyPrint() {
 		//Serial.print("Frame header: ");
@@ -107,7 +160,148 @@ struct DetectionDatagramm {
 	}
 };
 
-void printBytes(uint8_t* data, size_t len) {
+enum AckStatus : short {
+	SUCCESS = 0x0000, FAILURE = 0x0001,
+};
+
+struct ConfigurationCommandResponse
+{
+	byte HEADER[4]{ 0xFD, 0xFC, 0xFB, 0xFA };
+	short length;//should be 8
+	byte command[2];
+	AckStatus ackStatus;
+	byte protocollVersion[2];
+	byte buffer[2];
+	byte MFR[4]{ 4,3,2,1 };
+
+	static bool tryRead(byte* buffer, size_t bufferSize, ConfigurationCommandResponse* out) {
+		size_t window = 0;
+		while (window + sizeof(ConfigurationCommandResponse) < bufferSize) {
+			if (buffer[window] != 0xFD ||
+				buffer[window + 1] != 0xFC ||
+				buffer[window + 2] != 0xFB || 
+				buffer[window + 3] != 0xFA) {
+				window++;
+				continue;
+			}
+			short length = buffer[window + offsetof(ConfigurationCommandResponse, length) + 1] << 8 | buffer[window + offsetof(ConfigurationCommandResponse, length)];
+
+
+			if (length != 8) {
+				return false;
+			}
+			if (buffer[window + offsetof(ConfigurationCommandResponse, command)] != 0xFF || buffer[window + offsetof(ConfigurationCommandResponse, command) + 1] != 0x01)
+			{
+				//printBytes(buffer[window + offsetof(ConfigurationCommandResponse, command)], 2, -1);
+				return false;
+			}
+
+			
+
+			if (buffer[window + offsetof(ConfigurationCommandResponse, MFR)] != 0x04 ||
+				buffer[window + offsetof(ConfigurationCommandResponse, MFR) + 1] != 0x03 ||
+				buffer[window + offsetof(ConfigurationCommandResponse, MFR) + 2] != 0x02 ||
+				buffer[window + offsetof(ConfigurationCommandResponse, MFR) + 3] != 0x01)
+			{
+				return false;
+			}
+
+			memcpy(out, buffer + window, sizeof(ConfigurationCommandResponse));
+			return true;
+
+		}
+
+		return false;
+	}
+
+	void prettyPrint() {
+		Serial.print("ConfigurationCommandResponse: ");
+		Serial.print("Ack status: ");
+		switch (ackStatus)
+		{
+		case SUCCESS:
+			Serial.println("SUCCESS");
+			break;
+		case FAILURE:
+			Serial.println("FAILURE");
+			break;
+		default:
+			Serial.println("UNKNOWN");
+			break;
+		}
+	}
+};
+
+struct FirmwareCommandResponse {
+	byte HEADER[4]{ 0xFD, 0xFC, 0xFB, 0xFA };
+	short length;//should be 12
+	byte command[2];
+	AckStatus ackStatus;
+	byte firmwareType[2];
+	byte majorVersion[2];
+	byte minorVersion[4];
+	byte MFR[4]{ 4,3,2,1 };
+
+
+	static bool tryRead(byte* buffer, size_t bufferSize, FirmwareCommandResponse* out) {
+		size_t window = 0;
+		while (window + sizeof(FirmwareCommandResponse) < bufferSize) {
+			if (buffer[window] != 0xFD ||
+				buffer[window + 1] != 0xFC ||
+				buffer[window + 2] != 0xFB ||
+				buffer[window + 3] != 0xFA) {
+				window++;
+				continue;
+			}
+			short length = buffer[window + offsetof(FirmwareCommandResponse, length) + 1] << 8 | buffer[window + offsetof(FirmwareCommandResponse, length)];
+			
+			if (length != 12) {
+				return false;
+			}
+
+			if (buffer[window + offsetof(FirmwareCommandResponse, MFR)] != 0x04 ||
+				buffer[window + offsetof(FirmwareCommandResponse, MFR) + 1] != 0x03 ||
+				buffer[window + offsetof(FirmwareCommandResponse, MFR) + 2] != 0x02 ||
+				buffer[window + offsetof(FirmwareCommandResponse, MFR) + 3] != 0x01)
+			{
+				return false;
+			}
+
+			memcpy(out, buffer + window, sizeof(FirmwareCommandResponse));
+			return true;
+
+		}
+
+		return false;
+	}
+
+	void prettyPrint() {
+		Serial.print("FirmwareCommandResponse: ");
+		Serial.print("V");
+		Serial.print(majorVersion[1], HEX);
+		Serial.print(".");
+		printByte(majorVersion[0]);
+
+		Serial.print(".");
+		printByte(minorVersion[3]);
+		printByte(minorVersion[2]);
+		printByte(minorVersion[1]);
+		printByte(minorVersion[0]);
+
+		Serial.println();
+	}
+};
+
+
+void printByte(byte b) {
+	if (b < 16)
+	{
+		Serial.print("0");
+	}
+	Serial.print(b, HEX);
+}
+
+void printBytes(const uint8_t* data, size_t len, int highlightAt) {
 	for (size_t i = 0; i < len; i++)
 	{
 		if (data[i] < 16) {
@@ -115,17 +309,25 @@ void printBytes(uint8_t* data, size_t len) {
 		}
 		Serial.print(data[i],HEX);
 	}
+	if (highlightAt > -1 && highlightAt < len) {
+		Serial.println();
+		for (size_t i = 0; i < highlightAt; i++)
+		{
+			Serial.print("  ");
+		}
+		Serial.println("^^");
+	}
 }
 
 
-void sendMessage(void* buf, unsigned short size) {
+void sendMessage(void* buf, short size) {
 	ldSerial.write(0xFD);
 	ldSerial.write(0xFC);
 	ldSerial.write(0xFB);
 	ldSerial.write(0xFA);
 
-	ldSerial.write((char*) &size, 2);
-	ldSerial.write((char*) buf, size);
+	ldSerial.write((byte*) &size, 2);
+	ldSerial.write((byte*) buf, size);
 
 	ldSerial.write(0x04);
 	ldSerial.write(0x03);
@@ -133,9 +335,20 @@ void sendMessage(void* buf, unsigned short size) {
 	ldSerial.write(0x01);
 	ldSerial.flush();
 }
+
 struct {
-	byte payload[2]{ 0x0A, 0x00 };
+	byte payload[2]{ 0xA0, 0x00 };
 } GET_FIRMWARE;
+
+struct {
+	byte commandWord[2]{ 0xFF, 0x00 };
+	byte commandValue[2]{ 0x01, 0x00 };
+} ENABLE_CONFIG_COMMANDS;
+
+struct {
+	byte commandWord[2]{ 0xFE, 0x00 };
+} DISABLE_CONFIG_COMMANDS;
+
 
 void setup() {
 	Serial.begin(115200);
@@ -146,52 +359,88 @@ void setup() {
 	pinMode(6, INPUT);
 	pinMode(5, OUTPUT);
 
-	Serial.write("initializing ldserial");
+	Serial.println("initializing ldserial");
 
-	ldSerial.begin(9600);
-	Serial.write("Initialized ldserial");
-	//sendMessage(&GET_FIRMWARE, 2);
+	ldSerial.begin(57600);
+	Serial.println("Initialized ldserial");
 
 }
 
-long counter = 0;
 
+byte buffer[50]{ 0 };
+
+unsigned int cursor = 0;
 
 // the loop function runs over and over again forever
 void loop() {
-	while (ldSerial.available() == 0);
+	if (Serial.available())
+	{
+		auto command = Serial.read();
+		if (command == 'a') {
+			Serial.print("Got command: ");
+			Serial.println(command);
+			sendMessage(&ENABLE_CONFIG_COMMANDS, (short)sizeof(ENABLE_CONFIG_COMMANDS));
+		}		
+		if (command == 'b') {
+			Serial.print("Got command: ");
+			Serial.println(command);
+			sendMessage(&DISABLE_CONFIG_COMMANDS, (short)sizeof(DISABLE_CONFIG_COMMANDS));
+		}
+		if (command == 'c') {
+			Serial.print("Got command: ");
+			Serial.println(command);
+			sendMessage(&GET_FIRMWARE, (short)sizeof(GET_FIRMWARE));
+		}
+	}
 
-	DetectionDatagramm dd;
-	unsigned char buffer[sizeof(DetectionDatagramm)] {};
-	byte c = ldSerial.readBytes((unsigned char*) & buffer, sizeof(buffer));
-	//printBytes(buffer, sizeof(buffer));
-	memcpy(&dd, &buffer, sizeof(buffer));
-	dd.prettyPrint();
+	auto available = ldSerial.available();
+	if (available > 0) {
+		if (cursor == sizeof(buffer) - 1) {
+			for (size_t i = 0; i < sizeof(buffer) - 1; i++)
+			{
+				buffer[i] = buffer[i + 1];
+			}
 
+			buffer[cursor] = ldSerial.read();
+		}
+		else {
+			buffer[cursor] = ldSerial.read();
+			cursor++;
+		}
 
+		DetectionDatagramm dd;
+		auto isDetectionDatagramm = DetectionDatagramm::tryRead(buffer, sizeof(buffer), &dd);
+		
+		if (isDetectionDatagramm) {
+			dd.prettyPrint();
+			Serial.println("Resetting buffer");
+			memset(buffer, 0, sizeof(buffer));
+			cursor = 0;
+			return;
+		}
 
-	//Datagramm d = Datagramm();
+		ConfigurationCommandResponse cCR;
+		auto isConfigurationCommandResponse = ConfigurationCommandResponse::tryRead(buffer, sizeof(buffer), &cCR);
+		if (isConfigurationCommandResponse) {
+			cCR.prettyPrint();
+			Serial.println("Resetting buffer");
+			memset(buffer, 0, sizeof(buffer));
+			cursor = 0;
+		}
 
-	//byte buffer[50] = {};
+		FirmwareCommandResponse fCR;
+		auto isFirmwareCommandResponse = FirmwareCommandResponse::tryRead(buffer, sizeof(buffer), &fCR);
+		if (isFirmwareCommandResponse) {
+			fCR.prettyPrint();
+			Serial.println("Resetting buffer");
+			memset(buffer, 0, sizeof(buffer));
+			cursor = 0;
+		}
+	}
+	
+	
 
-	//auto len = ldSerial.readBytes((uint8_t*)&d, sizeof(Datagramm));
-	//auto len = ldSerial.readBytes((uint8_t*) &buffer, sizeof(buffer));
-
-	//Serial.print("Read bytes: ");
-	//Serial.println(len);
-	//Serial.print("Overflow:");
-	//Serial.println(ldSerial.overflow());
-
-	//for (size_t i = 0; i < sizeof(buffer); i++)
-	//{
-	//	Serial.print(" ");
-	//	if (((unsigned char*) &buffer)[i] < 16) {
-	//		Serial.print(0);
-	//	}
-	//	Serial.print(((unsigned char*)&buffer)[i], HEX);
-	//
-	//}
-	Serial.println();
-
+	//printBytes(buffer, sizeof(buffer), -1);
+	//Serial.println();
 }
 
